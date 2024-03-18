143f920d1cfcdde8848e6da57c33b84ffe2da030
N1zar0 <126731855+N1zar0@users.noreply.github.com>
2024-03-17 22:58:08 -0400

    Add files via upload

---
+++ groupprojectgui/groupprojectgui/Booking.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace groupproject
+{
+    class Booking
+    {
+       private int bookingID;
+       Customer customer;
+       Flight flight;
+       private string date;
+
+        public Booking(int bookingID, Customer customer, Flight flight, string date)
+        {
+            bookingID = 100;
+        }
+
+        public override string ToString()
+        {
+            string s = "Booking ID: " + bookingID;
+            s += "Date: " + date;
+            return s;
+        }
+
+    }
+}
